b04299b07723a8d049daf36fbdf94d0d2f58a4cc
binov <bessam.benyahya@gmail.com>
2024-01-08 15:13:04 +0100

    remove clerk

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
 import '@/app/globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-})
 
 export const metadata: Metadata = {
-  title: 'Admin | Smart Commerce for Franchises',
+  title: 'Smart Commerce for Franchises',
   description: 'Created by Binov company',
 }
 
@@ -20,11 +11,5 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={poppins.className}>{children}</body>
-      </html>
-    </ClerkProvider>
-  )
+  return <div>{children}</div>
 }
